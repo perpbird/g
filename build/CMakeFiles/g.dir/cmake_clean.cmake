9d585ae31632a03ec7ba5537797fd1db3c7b9968
perpbird <perpbird@gmail.com>
2026-02-15 16:22:48 +0300

    init commit

---
+++ build/CMakeFiles/g.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g.dir/src/Renderer/ShaderRenderer.cpp.o"
+  "CMakeFiles/g.dir/src/Renderer/ShaderRenderer.cpp.o.d"
+  "CMakeFiles/g.dir/src/Resource/ResourceManager.cpp.o"
+  "CMakeFiles/g.dir/src/Resource/ResourceManager.cpp.o.d"
+  "CMakeFiles/g.dir/src/main.cpp.o"
+  "CMakeFiles/g.dir/src/main.cpp.o.d"
+  "g"
+  "g.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
